dd851ae5d40821500745635cfe5076bcd66059ae
GetieBalew24 <getiebalew2020@gmail.com>
2022-10-04 23:59:00 -0700

    Insertion sort function

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+void swap(listint_t **start, listint_t *node1, listint_t *node2);
+/**
+ * insertion_sort_list - sorts a doubly linked list with
+ * the insertion sort algorithm
+ *
+ * @list: list to be sorted
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *temp1;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (temp = (*list)->next; temp && temp->prev; temp = temp->next)
+	{
+		for (; temp && temp->prev && temp->n < temp->prev->n;
+		     temp = temp->prev)
+		{
+			temp1 = temp->prev;
+			swap(list, temp1, temp);
+			print_list(*list);
+			temp = temp->next;
+		}
+	}
+}
+
+/**
+ * swap - swaps two nodes
+ * @start: the start/head node
+ * @node1: The first node
+ * @node2: the second node
+ *
+ * Return: void
+ */
+void swap(listint_t **start, listint_t *node1, listint_t *node2)
+{
+	listint_t *prev, *next;
+
+	prev = node1->prev;
+	next = node2->next;
+
+	if (prev != NULL)
+		prev->next = node2;
+	else
+		*start = node2;
+
+	node1->prev = node2;
+	node1->next = next;
+	node2->prev = prev;
+	node2->next = node1;
+	if (next)
+		next->prev = node1;
+}
